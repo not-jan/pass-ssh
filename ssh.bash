004843feb82f3e48dda3af7fd0620243cb28ac18
Tsogoo <tsogi.d@gmail.com>
2022-01-17 14:13:18 +0800

    if ssh address is not present, make one from user and IP

--- ssh.bash
+++ ssh.bash
@@ -15,15 +15,28 @@ ssh_read_data() {
   read -r ssh_password < <(echo "$contents")
 
   while read -r line; do
-  if [[ "$line" == ssh:* ]]; then
-    local ssh_address=${line#"ssh:"}
-    ssh_address=${ssh_address##+([[:space:]])}
-  fi
-  if [[ "$line" == sshflags:* ]]; then
-    local ssh_flags=${line#"sshflags:"}
-    ssh_flags=${ssh_flags##+([[:space:]])}
-  fi
+    if [[ "$line" == ssh:* ]]; then
+      local ssh_address=${line#"ssh:"}
+      ssh_address=${ssh_address##+([[:space:]])}
+    fi
+    if [[ "$line" == sshflags:* ]]; then
+      local ssh_flags=${line#"sshflags:"}
+      ssh_flags=${ssh_flags##+([[:space:]])}
+    fi
+    if [[ "$line" == user:* ]]; then
+      local ssh_user=${line#"user:"}
+#      ssh_user=${ssh_user##+([[:space::]])}
+    fi
+    if [[ "$line": == IP:* ]]; then
+      local ssh_host=${line#"IP:"}
+#      ssh_host=${ssh_host##+([[:space::]])}
+    fi
   done < <(echo "$contents")
+  if [[ -z ${ssh_address+x} ]]; then
+    ssh_address="${ssh_user// /}@${ssh_host// /}"
+    echo "$ssh_address"
+  fi
+
   [[ -z ${ssh_address+x} ]] && die "$path: SSH host not defined."
   # Default value for flags
   ssh_flags="${ssh_flags:-}"
